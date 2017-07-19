576d12f7c4796cb382924ce6013e44289807e089
amyible <amyible@gmail.com>
2017-07-19 14:17:49 -0700

    static version

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>hellooooo!</p>,
+const dummyData = ['Cook dinner', 'Do laundry', 'Go to Gym', 'Mail Letters'];
+
+class Todo extends React.Component {
+  render() {
+    return (
+      <li><button>X</button>{this.props.task}</li>
+    )
+  }
+}
+
+class ToDoList extends React.Component {
+  render() {
+    let todos = dummyData.map((item, i) => (<Todo key={i} task={item} />))
+    return (
+      <ul>{todos}</ul>
+    )
+  }
+}
+
+ReactDOM.render(<ToDoList />,
    document.getElementById('root'));
